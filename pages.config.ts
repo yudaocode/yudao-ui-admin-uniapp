abad67d5d0cb7d021f0bcba0331ef1fdd366f30a
YunaiV <zhijiantianya@gmail.com>
2025-12-09 18:45:39 +0800

    feat：增加 wot 集成，参考 https://unibest.tech/base/7-ui#%E5%8D%B8%E8%BD%BD-wot-ui-%E5%BA%93 文档

--- pages.config.ts
+++ pages.config.ts
@@ -15,6 +15,7 @@ export default defineUniPages({
       '^fg-(.*)': '@/components/fg-$1/fg-$1.vue',
       '^(?!z-paging-refresh|z-paging-load-more)z-paging(.*)':
         'z-paging/components/z-paging$1/z-paging$1.vue',
+      '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
     },
   },
   // tabbar 的配置统一在 “./src/tabbar/config.ts” 文件中
