6a6eeaf5bbd772be325374bdd15e36243cca013d
YunaiV <zhijiantianya@gmail.com>
2026-07-10 23:14:08 +0800

    chore(eslint): 忽略非项目维护的 lime-echart 组件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,8 @@ export default uniHelper({
     'src/manifest.json',
     // 忽略自动生成文件
     'src/service/**',
+    // 忽略非项目维护的 lime-echart 组件
+    'src/pages-statistics/components/lime-echart/**',
   ],
   // https://eslint-config.antfu.me/rules
   rules: {
